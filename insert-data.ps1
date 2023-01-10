2a34d09e2bf57b86528885f364d742af449a17f5
AgathePons <ponsagathe37@gmail.com>
2023-01-10 17:09:56 +0100

    update script file to run all sql files

--- insert-data.ps1
+++ insert-data.ps1
@@ -1,4 +1,4 @@
-echo off
+# echo off
 # docker compose -p dbpoe-pg-app exec -T db psql -U poe -d dbpoe < .\poe.sql
 # docker compose -p dbpoe-pg-app exec -T db psql -U poe -d dbpoe < .\trainee.sql
 Get-Content data-poe.sql | docker compose -p dbpoe-pg-app exec -T db psql -U poe dbpoe
