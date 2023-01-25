0ff616c2da4ecea83c8a91768c8c074cf794a280
AgathePons <ponsagathe37@gmail.com>
2023-01-25 15:35:57 +0100

    modify survey/question/answer

--- data-answer.sql
+++ data-answer.sql
@@ -1,27 +1,54 @@
 SET client_encoding = 'UTF8';
 -- survey 1
-insert into answer(id, text, question_id, order_in_question) values (1, 'Jean-Luc', 2, 0);
-insert into answer(id, text, question_id, order_in_question) values (2, 'Matthias', 2, 1);
-insert into answer(id, text, question_id, order_in_question) values (3, 'Clementine', 2, 2);
-insert into answer(id, text, question_id, order_in_question) values (4, 'Marie', 2, 3);
-insert into answer(id, text, question_id, order_in_question) values (5, 'Ludo', 2, 4);
-insert into answer(id, text, question_id, order_in_question) values (6, '20k - 25k brut/an', 4, 0);
-insert into answer(id, text, question_id, order_in_question) values (7, '25k - 30k brut/an', 4, 1);
-insert into answer(id, text, question_id, order_in_question) values (8, '30k - 35k brut/an', 4, 2);
-insert into answer(id, text, question_id, order_in_question) values (9, '35k - 40k brut/an', 4, 3);
-insert into answer(id, text, question_id, order_in_question) values (10, '45k - 50k brut/an', 4, 4);
-insert into answer(id, text, question_id, order_in_question) values (11, '+ 50k brut/an', 4, 5);
+
+insert into answer(id, text, question_id, order_in_question) values (1, 'Chez votre employeur', 11, 0);
+insert into answer(id, text, question_id, order_in_question) values (2, 'Chez le client', 11, 1);
+insert into answer(id, text, question_id, order_in_question) values (3, 'Sur les deux sites', 11, 2);
+
 -- survey 2
-insert into answer(id, text, question_id, order_in_question) values (12, 'Vraiment pas bien', 8, 0);
-insert into answer(id, text, question_id, order_in_question) values (13, 'Encore debutant', 8, 1);
-insert into answer(id, text, question_id, order_in_question) values (14, 'De plus en plus confiant', 8, 2);
-insert into answer(id, text, question_id, order_in_question) values (15, 'Tres bien', 8, 3);
+
+insert into answer(id, text, question_id, order_in_question) values (12, 'Pas d''integration a la fin de la periode d''essai', 15, 0);
+insert into answer(id, text, question_id, order_in_question) values (13, 'Interuption de la periode d''essai a votre initiative', 15, 1);
+insert into answer(id, text, question_id, order_in_question) values (14, 'Interruption de la periode d''essai a l''initiative de l''entreprise', 15, 2);
+insert into answer(id, text, question_id, order_in_question) values (15, 'Changement d''entreprise', 15, 3);
+
+insert into answer(id, text, question_id, order_in_question) values (16, 'Angular', 17, 0);
+insert into answer(id, text, question_id, order_in_question) values (17, 'React', 17, 1);
+insert into answer(id, text, question_id, order_in_question) values (18, 'Vue', 17, 2);
+insert into answer(id, text, question_id, order_in_question) values (19, 'JS Natif', 17, 3);
+insert into answer(id, text, question_id, order_in_question) values (20, 'Autre framework frontend', 17, 4);
+insert into answer(id, text, question_id, order_in_question) values (21, 'Spring boot', 17, 5);
+insert into answer(id, text, question_id, order_in_question) values (22, 'JEE natif', 17, 6);
+insert into answer(id, text, question_id, order_in_question) values (23, 'Symfony', 17, 7);
+insert into answer(id, text, question_id, order_in_question) values (24, 'Laravel', 17, 8);
+insert into answer(id, text, question_id, order_in_question) values (25, 'PHP natif', 17, 9);
+insert into answer(id, text, question_id, order_in_question) values (26, 'Autre framework PHP', 17, 10);
+insert into answer(id, text, question_id, order_in_question) values (27, 'Python', 17, 11);
+insert into answer(id, text, question_id, order_in_question) values (28, '.Net', 17, 12);
+insert into answer(id, text, question_id, order_in_question) values (29, 'Autre tecnologies', 17, 13);
+
 -- survey 3
-insert into answer(id, text, question_id, order_in_question) values (16, 'Spring Boot', 12, 0);
-insert into answer(id, text, question_id, order_in_question) values (17, 'Angular', 12, 1);
-insert into answer(id, text, question_id, order_in_question) values (18, 'React', 12, 2);
-insert into answer(id, text, question_id, order_in_question) values (19, 'Symfony', 12, 3);
-insert into answer(id, text, question_id, order_in_question) values (20, 'NodeJs', 12, 4);
-insert into answer(id, text, question_id, order_in_question) values (21, 'Flutter', 12, 5);
-insert into answer(id, text, question_id, order_in_question) values (22, 'Autres', 12, 6);
+
+insert into answer(id, text, question_id, order_in_question) values (30, 'Angular', 24, 0);
+insert into answer(id, text, question_id, order_in_question) values (31, 'React', 24, 1);
+insert into answer(id, text, question_id, order_in_question) values (32, 'Vue', 24, 2);
+insert into answer(id, text, question_id, order_in_question) values (33, 'JS Natif', 24, 3);
+insert into answer(id, text, question_id, order_in_question) values (34, 'Autre framework frontend', 24, 4);
+insert into answer(id, text, question_id, order_in_question) values (35, 'Spring boot', 24, 5);
+insert into answer(id, text, question_id, order_in_question) values (36, 'JEE natif', 24, 6);
+insert into answer(id, text, question_id, order_in_question) values (37, 'Symfony', 24, 7);
+insert into answer(id, text, question_id, order_in_question) values (38, 'Laravel', 24, 8);
+insert into answer(id, text, question_id, order_in_question) values (39, 'PHP natif', 24, 9);
+insert into answer(id, text, question_id, order_in_question) values (40, 'Autre framework PHP', 24, 10);
+insert into answer(id, text, question_id, order_in_question) values (41, 'Python', 24, 11);
+insert into answer(id, text, question_id, order_in_question) values (42, '.Net', 24, 12);
+insert into answer(id, text, question_id, order_in_question) values (43, 'Autre tecnologies', 24, 13);
+
+insert into answer(id, text, question_id, order_in_question) values (44, 'DevOps', 27, 0);
+insert into answer(id, text, question_id, order_in_question) values (45, 'Admin Sys', 27, 1);
+insert into answer(id, text, question_id, order_in_question) values (46, 'Chef de projet', 27, 2);
+insert into answer(id, text, question_id, order_in_question) values (47, 'Scrum Master', 27, 3);
+insert into answer(id, text, question_id, order_in_question) values (48, 'Product Owner', 27, 4);
+insert into answer(id, text, question_id, order_in_question) values (49, 'UX/UI designer', 27, 5);
+insert into answer(id, text, question_id, order_in_question) values (50, 'Pas d''evolution', 27, 6);
 Select setval('answer_id_seq', max(id)) from answer;
--- data-question.sql
+++ data-question.sql
@@ -2,43 +2,87 @@ SET client_encoding = 'UTF8';
 -- survey 1
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (1, 'Comment vous appelez-vous ?', 'FREE', 0, 1);
+values (1, 'Comment s''est deroulee votre integration chez votre nouvel employeur ?', 'FREE', 0, 1);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (2, 'Quels professeurs avez-vous eu ?', 'CHOOSE_MANY', 1, 1);
+values (2, 'Comment s''est deroulee votre integration chez le client ? ', 'FREE', 1, 1);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (3, 'Etes-vous satisfait de votre formation ?', 'YES_NO', 2, 1);
+values (3, 'Etes-vous satisfait des missions confiees ?', 'FREE', 2, 1);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (4, 'Sur quelle fourchette de salaire vous situez-vous ?', 'CHOOSE_ONE', 3, 1);
+values (4, 'Les missions confiees correspondent-elles a la formation que vous avez recue ?', 'FREE', 3, 1);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (5, 'Etes-vous toujours dans votre entreprise ?', 'YES_NO', 4, 1);
+values (5, 'Rencontrez-vous des difficultes dans les missions qui vous sont confiees ?', 'FREE', 4, 1);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (6, 'Comment evaluez-vous votre accompagnement sur vos projets (tuteur, manager, équipe, ...) ?', 'FREE', 5, 1);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (7, 'Comment evaluez-vous votre relation avec l''equipe projet ?', 'FREE', 6, 1);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (8, 'Comment evaluez-vous votre relation avec votre employeur', 'FREE', 7, 1);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (9, 'Comment evaluez-vous votre relation avec votre manager Client', 'FREE', 8, 1);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (10, 'Comment evaluez-vous votre relation avec votre employeur', 'FREE', 9, 1);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (11, 'Sur quel site travaillez-vous ?', 'CHOOSE_ONE', 10, 1);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (12, 'Avez-vous ete integre avec un autre membre de votre promotion POE ?', 'YES_NO', 11, 1);
+
 -- survey 2
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (6, 'Comment vous appelez-vous ?', 'FREE', 0, 2);
+values (13, 'Etes-vous toujours en poste au sein de l''entreprise qui vous a initialement recrute ?', 'YES_NO', 0, 2);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (7, 'Etes-vous toujours dans votre entreprise ?', 'YES_NO', 1, 2);
+values (14, 'Sinon, a quelle date etes vous parti ?', 'FREE', 1, 2);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (8, 'Comment vous sentez-vous dans votre travail ?', 'CHOOSE_ONE', 2, 2);
+values (15, 'Si vous etes parti, quelle en est la raison ?', 'CHOOSE_ONE', 2, 2);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (9, 'Avez-vous une remarque a faire pour nous aider a vous accompagner dans l''apres formation ?', 'FREE', 3, 2);
+values (16, 'Combien de mission differentes avez-vous effectue depuis votre integration ?', 'FREE', 3, 2);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (17, 'Quelles technologies avez-vous utilise au cours de vos differentes missions ?', 'CHOOSE_MANY', 4, 2);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (18, 'Avez-vous eu recours a des formations complementaires ?', 'YES_NO', 5, 2);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (19, 'Sur une echelle de 1 a 10 comment estimez-vous votre propre progression en tant que Fullstack durant ces 6 mois ?', 'FREE', 6, 2);
+
 -- survey 3
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (10, 'Comment vous appelez-vous ?', 'FREE', 0, 3);
+values (20, 'Etes-vous toujours en poste au sein de l''entreprise qui vous a initialement recrute ?', 'YES_NO', 0, 3);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (11, 'Etes-vous toujours dans votre entreprise ?', 'YES_NO', 1, 3);
+values (21, 'Sinon, a quelle date etes vous parti ?', 'FREE', 1, 3);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (12, 'Sur quelles technos travaillez-vous maintenant', 'CHOOSE_MANY', 2, 3);
+values (22, 'Si vous etes parti, quelle en est la raison ?', 'FREE', 2, 3);
 insert into question 
 (id, text, answer_type, order_in_survey, survey_id) 
-values (13, 'Accepteriez-vous de nous mettre un avis sur Google ?', 'YES_NO', 3, 3);
+values (23, 'Combien de mission differentes avez-vous effectue depuis votre integration ?', 'FREE', 3, 3);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (24, 'Quelles technologies avez-vous utilise au cours de vos differentes missions ?', 'CHOOSE_MANY', 4, 3);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (25, 'Avez-vous eu recours a des formations complementaires ?', 'YES_NO', 5, 3);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (26, 'Sur une echelle de 1 a 10 comment estimez-vous votre propre progression en tant que Fullstack durant ces 6 mois ?', 'FREE', 6, 3);
+insert into question 
+(id, text, answer_type, order_in_survey, survey_id) 
+values (27, 'Avez-vous evolue vers un autre type de mission ?', 'CHOOSE_ONE', 7, 3);
 Select setval('question_id_seq', max(id)) from question;
